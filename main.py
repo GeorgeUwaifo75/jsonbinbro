70d01f32881cec4a372a68a5a88bdd65fc123e19
George Uwaifo <geocorpsys@gmail.com>
2026-05-28 23:37:57 +0100

    Add modified application files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, JSONResponse
+
 from datetime import datetime
 from typing import List, Dict, Any, Optional
 from pydantic import BaseModel
